66c46add6f03f074ca9f406e7eed2f8beeaee691
Frank Parejo <frank.parejo@sympower.net>
2024-08-09 17:58:04 +0200

    reduce redis timeout to 10 seconds

--- api/src/main.rs
+++ api/src/main.rs
@@ -7,13 +7,15 @@ mod telemetry;
 use anyhow::Error;
 use axum::Router;
 use bb8_redis::RedisConnectionManager;
-use std::sync::Arc;
+use std::{sync::Arc, time::Duration};
 use tower_http::cors::{Any, CorsLayer};
 
 use config::get_app_settings;
 use github::router::GithubRepositoryRouter;
 use state::AppState;
 use telemetry::{get_subscriber, init_subscriber};
+
+const REDIS_POOL_CONNECTION_TIMEOUT: u64 = 10;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
@@ -31,7 +33,11 @@ async fn main() -> Result<(), Error> {
     let redis_settings = settings.redis;
 
     let redis_manager = RedisConnectionManager::new(redis_settings.url).unwrap();
-    let redis_pool = bb8::Pool::builder().build(redis_manager).await.unwrap();
+    let redis_pool = bb8::Pool::builder()
+        .connection_timeout(Duration::from_secs(REDIS_POOL_CONNECTION_TIMEOUT))
+        .build(redis_manager)
+        .await
+        .unwrap();
 
     let state = Arc::new(AppState {
         github_settings,
